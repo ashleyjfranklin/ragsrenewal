73d15ee83d643838b5f821d84f6279c5fed276c9
Ashley Franklin <ashleyfranklin1991@gmail.com>
2019-03-26 23:21:13 +0000

    Updates

---
+++ precache-manifest.72648fe01a82ac7c352a4827d0ae9a49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "51e70e9b87778b002c21ef61c4f5d350",
+    "url": "/static/media/MainTitleImage.51e70e9b.jpeg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8f86a5d4c7b913478def",
+    "url": "/static/js/main.d90b1981.chunk.js"
+  },
+  {
+    "revision": "e4a62e95c0164d6f955c",
+    "url": "/static/js/2.46be0faf.chunk.js"
+  },
+  {
+    "revision": "8f86a5d4c7b913478def",
+    "url": "/static/css/main.502a5d6d.chunk.css"
+  },
+  {
+    "revision": "6320a408c701676dcbf90f62485c0c31",
+    "url": "/index.html"
+  }
+];
